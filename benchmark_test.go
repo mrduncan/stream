81897adbca82733da232a7cfff94d7402922262b
Matt Duncan <matt@mattduncan.org>
2015-05-14 14:22:43 -0700

    Parallelize benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -18,15 +18,16 @@ func benchmarkObserve(b *testing.B, capacity int, distinct uint64) {
 	r := rand.New(rand.NewSource(1))
 	zipf := rand.NewZipf(r, 1.5, 5, distinct)
 
-	data := make([]string, b.N)
+	items := make(chan string, b.N)
 	for i := 0; i < b.N; i++ {
-		data[i] = strconv.FormatUint(zipf.Uint64(), 10)
+		items <- strconv.FormatUint(zipf.Uint64(), 10)
 	}
 
 	summary := NewSummary(capacity)
-
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		summary.Observe(data[i])
-	}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			summary.Observe(<-items)
+		}
+	})
 }
