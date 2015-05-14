6216dc5dc1a68bfaf3669a894f237d184a2ca46d
Matt Duncan <matt@mattduncan.org>
2015-05-13 19:33:30 -0700

    Simplify test cases.

--- stream_test.go
+++ stream_test.go
@@ -1,77 +1,43 @@
 package stream
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestTopZeroIsEmpty(t *testing.T) {
-	summary := NewSummary(1)
-	assertEqualInt(t, len(summary.Top(0)), 0)
-}
-
-func TestTopNotLargerThanCapacity(t *testing.T) {
-	summary := NewSummary(1)
-	summary.Observe("item")
-	assertEqualInt(t, len(summary.Top(2)), 1)
-}
-
-func TestTopNotLargerThanN(t *testing.T) {
+func TestTopLength(t *testing.T) {
 	summary := NewSummary(2)
 	summary.Observe("one")
+	assertEqual(t, len(summary.Top(10)), 1)
+
 	summary.Observe("two")
-	assertEqualInt(t, len(summary.Top(1)), 1)
+	assertEqual(t, len(summary.Top(1)), 1)
+
+	assertEqual(t, len(summary.Top(0)), 0)
 }
 
 func TestTopOrderedDescending(t *testing.T) {
-	summary := NewSummary(2)
-	summary.Observe("once")
-	summary.Observe("twice")
-	summary.Observe("twice")
-	assertEqualString(t, summary.Top(2)[0].Item, "twice")
-	assertEqualString(t, summary.Top(2)[1].Item, "once")
-}
-
-func TestTopOrderedDescendingWithJump(t *testing.T) {
 	summary := NewSummary(3)
 	summary.Observe("once a")
 	summary.Observe("once b")
 	summary.Observe("twice")
 	summary.Observe("twice")
-	assertEqualString(t, summary.Top(3)[0].Item, "twice")
+	assertEqual(t, summary.Top(3)[0].Item, "twice")
+	assertEqual(t, summary.Top(3)[1].Item, "once a")
+	assertEqual(t, summary.Top(3)[2].Item, "once b")
 }
 
-func TestErrorRateWhenExceedingCapacity(t *testing.T) {
-	summary := NewSummary(2)
-	summary.Observe("zero")
-	summary.Observe("one")
-	summary.Observe("zero")
-	summary.Observe("two")
-	summary.Observe("zero")
-	assertEqualUint64(t, summary.Top(2)[1].ErrorRate, 1)
-}
-
-func TestCountWhenExceedingCapacity(t *testing.T) {
+func TestExceedCapacity(t *testing.T) {
 	summary := NewSummary(1)
 	summary.Observe("zero")
 	summary.Observe("one")
 	summary.Observe("zero")
-	assertEqualUint64(t, summary.Top(1)[0].Count, 3)
+	assertEqual(t, summary.Top(1)[0].Count, uint64(3))
+	assertEqual(t, summary.Top(1)[0].ErrorRate, uint64(2))
 }
 
-func assertEqualString(t *testing.T, actual, expected string) {
-	if actual != expected {
-		t.Errorf("Got %s but expected %s", actual, expected)
-	}
-}
-
-func assertEqualInt(t *testing.T, actual, expected int) {
-	if actual != expected {
-		t.Errorf("Got %d but expected %d", actual, expected)
-	}
-}
-
-func assertEqualUint64(t *testing.T, actual, expected uint64) {
-	if actual != expected {
-		t.Errorf("Got %d but expected %d", actual, expected)
+func assertEqual(t *testing.T, actual, expected interface{}) {
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Got %+v but expected %+v", actual, expected)
 	}
 }
